99cecc4620962c16d536cf833395135ff657fb2a
fzwqq <fzwqq@sina.cn>
2021-03-19 06:12:16 +0800

    update sub jobs

---
+++ sub_job/Jin_FG_v1/job_four_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -J Pp_SpCp_Jv0
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+JAVA_HOME=/seu_share/home/weiweiwu/wlocal/jdk/jdk-11.0.1
+#JRE_HOME=${JAVA_HOME}/jre
+CLASSPATH=.:${JAVA_HOME}/lib
+PATH=$JAVA_HOME/bin:$PATH
+PATH=/seu_share/home/weiweiwu/anaconda3/bin:$PATH
+LD_LIBRARY_PATH=/seu_share/home/weiweiwu/anaconda3/lib:$LD_LIBRARY_PATH
+export JAVA_HOME  CLASSPATH PATH 
+cd ~/uRTS_v2
+python3 test_jin.py  --algo ppo -lr 1e-5 --env-id  fullgame-v0 --render 0 --saving-prefix Pp_Sp_Jv0_ --ai1_socket Jin --ai2_socket Jin
+
---
+++ sub_job/Jin_FG_v1/job_six_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J Pp_SpCp_J6v1
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu num=1
+JAVA_HOME=/seu_share/home/weiweiwu/wlocal/jdk/jdk-11.0.1
+#JRE_HOME=${JAVA_HOME}/jre
+CLASSPATH=.:${JAVA_HOME}/lib
+PATH=$JAVA_HOME/bin:$PATH
+PATH=/seu_share/home/weiweiwu/anaconda3/bin:$PATH
+LD_LIBRARY_PATH=/seu_share/home/weiweiwu/anaconda3/lib:$LD_LIBRARY_PATH
+export JAVA_HOME  CLASSPATH PATH 
+
+cd ~/uRTS_v2/microrts
+python3 test_jin.py  --algo ppo -lr 1e-5 --env-id  fullgame-v1 --render 0 --saving-prefix Pp_SpCp_J6v1_ --ai1_socket Jin --ai2_socket Jin
+
