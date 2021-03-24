66138631f41aced692e56957614f6592bc008c19
fzwqq <fzwqq@sina.cn>
2021-03-24 14:21:46 +0800

    update

--- sub_job/multimaps_league/multimaps_sp.sh
+++ sub_job/multimaps_league/multimaps_sp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -J AI2
+#BSUB -J AI2_cp
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
@@ -12,5 +12,5 @@ PATH=/seu_share/home/weiweiwu/anaconda3/bin:$PATH
 LD_LIBRARY_PATH=/seu_share/home/weiweiwu/anaconda3/lib:$LD_LIBRARY_PATH
 export JAVA_HOME  CLASSPATH PATH 
 cd ~/uRTS_v2
-python3 test_paralell_jin.py --envs-id fullgame-v0,fullgame-v0,fullgame-v0,fullgame-v0,fullgame-v1,fullgame-v1,fullgame-v1,fullgame-v1,fullgame-v2,fullgame-v2,fullgame-v2,fullgame-v2,fullgame-v3,fullgame-v3,fullgame-v3,fullgame-v3 --league None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None --num-process 16 --algo ppo -lr 1e-4  --ai1_socket Jin --ai2_socket Jin --render 0 --saving-prefix multimaps_sp
+python3 test_paralell_jin.py --envs-id fullgame-v0,fullgame-v0,fullgame-v0,fullgame-v0,fullgame-v1,fullgame-v1,fullgame-v1,fullgame-v1,fullgame-v2,fullgame-v2,fullgame-v2,fullgame-v2,fullgame-v3,fullgame-v3,fullgame-v3,fullgame-v3 --league None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None --num-process 16 --algo ppo -lr 1e-4  --ai1_socket Jin --ai2_socket Jin --render 0 --saving-prefix multimaps_v2sp
 #python3 test_paralell.py  --algo ppo -lr 1e-4 --env-id  fullgame-v1 --render 0 --saving-prefix PPO_League_Six --league Random,WorkerRush,NaiveMCTS,UCT,MCTS,Passive --num-process 16
